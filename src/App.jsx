6de8cb03988967fadd7e9576ebb03a5faa1a99d7
Charels Bruno <charlesbrcosta@gmail.com>
2023-09-06 20:07:11 -0300

    aula inicial react - ignite

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Post } from './Post';
+export function App() {
+  return (
+    <>
+      <Post
+        author="Bruno"
+        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut tenetur nam molestias sit nobis assumenda odio autem dolorum quia, rerum velit expedita sed ipsum earum, consectetur neque corporis at! Laudantium."
+      />
+
+      <Post
+        author="B1tZer0"
+        content="Fortinite e CSGo"
+      />
+    </>
+  )
+}
+
+
