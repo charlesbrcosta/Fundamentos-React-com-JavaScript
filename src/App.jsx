d8e0cb10a226156a6558ab7059c92cd8d29b84b7
Charels Bruno <charlesbrcosta@gmail.com>
2023-09-11 15:09:39 -0300

    Sidebar Finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+import styles from './App.module.css'
 import './global.css';
 
 export function App() {
@@ -7,15 +9,22 @@ export function App() {
     <div>
       <Header />
 
-      <Post
-        author="Bruno"
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut tenetur nam molestias sit nobis assumenda odio autem dolorum quia, rerum velit expedita sed ipsum earum, consectetur neque corporis at! Laudantium."
-      />
+      <div className={styles.wrapper}>
 
-      <Post
-        author="B1tZer0"
-        content="Fortinite e CSGo"
-      />
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Bruno"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut tenetur nam molestias sit nobis assumenda odio autem dolorum quia, rerum velit expedita sed ipsum earum, consectetur neque corporis at! Laudantium."
+          />
+
+          <Post
+            author="B1tZer0"
+            content="Fortinite e CSGo"
+          />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,29 @@
+import styles from './Sidebar.module.css'
+import { PencilSimpleLine } from 'phosphor-react'
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1526040652367-ac003a0475fe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZGV2ZWxvcHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=50"
+            />
+            <div className={styles.profile}>
+                <img
+                    className={styles.avatar}
+                    src="https://github.com/charlesbrcosta.png"
+                />
+                <strong>Charles Bruno</strong>
+                <span>Web Developer</span>
+            </div>
+            <footer>
+
+                <a href="#">
+                    <PencilSimpleLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+
+}
