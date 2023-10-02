98e8c7c8e028a5183bdfa4535e422aaafc68417e
Charels Bruno <charlesbrcosta@gmail.com>
2023-10-01 22:26:08 -0300

    add componente Post

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
-import styles from './Header.module.css';
-import igniteLogo from '../assets/ignite-logo.svg';
-
+import styles from "./Header.module.css"
+import igniteLogo from "../assets/ignite-logo.svg"
 
 export function Header() {
-    return (
-        <header className={styles.header}>
-            <img src={igniteLogo} alt="Logo do Ignite" />
-        </header>
-    );
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logo do Ignite" />
+    </header>
+  )
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,38 @@
+import styles from "./Post.module.css"
+
+export function Post() {
+  return (
+    <article>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/charlesbrcosta.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Charles Bruno</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="01 de Outubro às 22:16" dateTime="2023-10-01 22:17:20">
+          {" "}
+          Publicado há 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return,
+        </p>{" "}
+        evento <p>da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>
+          <a href="#">👉 jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto #nlw #rocketseat</a>
+        </p>
+      </div>
+    </article>
+  )
+}
