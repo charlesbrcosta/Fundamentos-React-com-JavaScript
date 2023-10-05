de17ffdad9728dcd5d696fa87fa1275dffad8a3a
bruno <charlesbrcosta@gmail.com>
2023-10-05 07:16:59 -0300

    Adicionado estilização no Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,25 +2,31 @@ import styles from "./Post.module.css"
 
 export function Post() {
   return (
-    <article>
+    <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/charlesbrcosta.png"
-          />
+          <img className={styles.avatar} src="https://github.com/charlesbrcosta.png" />
           <div className={styles.authorInfo}>
             <strong>Charles Bruno</strong>
             <span>Web Developer</span>
           </div>
         </div>
-        <time title="01 de Outubro às 22:16" dateTime="2023-10-01 22:17:20">Publicado há 1h</time>
+        <time title="01 de Outubro às 22:16" dateTime="2023-10-01 22:17:20">
+          Publicado há 1h
+        </time>
       </header>
       <div className={styles.content}>
         <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fizno NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fizno
+          NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
         <p><a href="#">👉 jane.design/doctorcare</a></p>
-        <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+        <p>
+          <a href="#">#novoprojeto</a> {' '}
+          <a href="#">#nlw</a> {' '}
+          <a href="#">#rocketseat</a>
+        </p>
       </div>
     </article>
   )
