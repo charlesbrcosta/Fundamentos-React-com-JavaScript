9d5a9d443aeb3eb06483a11b7c69baf838c69949
bruno <charlesbrcosta@gmail.com>
2023-10-06 21:29:09 -0300

    estilizacao do form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,7 +5,10 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/charlesbrcosta.png" />
+          <img
+            className={styles.avatar}
+            src="https://github.com/charlesbrcosta.png"
+          />
           <div className={styles.authorInfo}>
             <strong>Charles Bruno</strong>
             <span>Web Developer</span>
@@ -21,13 +24,21 @@ export function Post() {
           Acabei de subir mais um projeto no meu portifa. É um projeto que fizno
           NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
         </p>
-        <p><a href="#">👉 jane.design/doctorcare</a></p>
         <p>
-          <a href="#">#novoprojeto</a> {' '}
-          <a href="#">#nlw</a> {' '}
+          <a href="#">👉 jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
           <a href="#">#rocketseat</a>
         </p>
       </div>
+      <form className={styles.commentForm}>
+        <textarea placeholder="Deixe seu comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
