bffd46d0e2dc543a1db8d71c4372b1f6c62271d7
bruno <charlesbrcosta@gmail.com>
2023-10-15 13:11:40 -0300

    ajuste de  erros de renderizacao nos componentes com list

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -6,20 +6,19 @@ import styles from "./Comment.module.css"
 
 export function Comment({ content }) {
   return (
-    <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://www.github.com/charlesbrcosta.png" alt="" />
-      <div className={styles.commentBox}>
-        <div className={styles.commentContent}>
+    <div className={ styles.comment }>
+      <Avatar hasBorder={ false } src="https://www.github.com/charlesbrcosta.png" alt="" />
+      <div className={ styles.commentBox }>
+        <div className={ styles.commentContent }>
           <header>
-            <div className={styles.authorAndTime}>
+            <div className={ styles.authorAndTime }>
               <strong>Charles Bruno</strong>
-              <time title="09 de outubro às 20:47" dateTime="2023-10-09 20:48:00" >
-                {" "}
+              <time title="09 de outubro às 20:47" dateTime="2023-10-09 20:48:00" >{" "}
                 Há cerca de 1 hora{" "}
               </time>
             </div>
             <button title="Deletar comentário">
-              <Trash size={24} />
+              <Trash size={ 24 } />
             </button>
           </header>
           <p>{ content }</p>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -53,9 +53,9 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === "paragraph") {
-            return <p>{line.content}</p>
+            return <p key={ line.content }>{line.content}</p>
           } else if (line.type === "link") {
-            return <p><a href="#">{line.content}</a></p>
+            return <p key={ line.content }><a href="#">{line.content}</a></p>
           }
         })}
       </div>
@@ -79,10 +79,11 @@ export function Post({ author, publishedAt, content }) {
         {comments.map(comment => {
           return (
             <Comment 
-              content = {comment}
+              key={ comment }
+              content = { comment }
             />
           )
-        })}        
+        })}    
       </div>
     </article>
   )
