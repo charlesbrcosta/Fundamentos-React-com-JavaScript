d23640b29bee66b4ca4e1de2eba5d35b0f73f14c
bruno <charlesbrcosta@gmail.com>
2023-10-16 07:25:39 -0300

    add função like nos comentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,14 +1,24 @@
 import { Trash } from "phosphor-react"
 import { ThumbsUp } from "phosphor-react"
 import { Avatar } from "./Avatar"
+import { useState } from "react"
 
 import styles from "./Comment.module.css"
 
+
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
 
   function handleDeleteComment(){
     onDeleteComment(content);
+  };
+
+  function handleLikeComment(){
+    setLikeCount((state) => {
+      return state + 1
+    });        
   }
+
 
   return (
     <div className={ styles.comment }>
@@ -29,9 +39,9 @@ export function Comment({ content, onDeleteComment }) {
           <p>{ content }</p>
         </div>
         <footer>
-          <button>
+          <button onClick={ handleLikeComment }>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{ likeCount }</span>
           </button>
         </footer>
       </div>
